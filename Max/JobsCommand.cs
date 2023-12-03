ff44f8d6464c71963a673856728c5c96b726fb3b
Kelly Elton <its.the.doc@gmail.com>
2023-12-03 00:38:48 -0600

    list backup jobs

---
+++ Max/JobsCommand.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.CommandLine.IO;
+using System.Text.Json;
+using Spectre.Console;
+
+namespace max
+{
+    public sealed class JobsCommand : Command, ICommandHandler
+    {
+        private readonly Option<string> _config_path;
+
+        public JobsCommand(Option<string> config_path)
+            : base("jobs", "List the backup jobs")
+        {
+            _config_path = config_path ?? throw new ArgumentNullException(nameof(config_path));
+        }
+
+        public int Invoke(InvocationContext context) {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> InvokeAsync(InvocationContext context) {
+            var config_file_path = context.BindingContext.ParseResult.GetValueForOption(_config_path);
+
+            if (string.IsNullOrWhiteSpace(config_file_path)) {
+                // TODO: Shouldn't duplicate this line
+                config_file_path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "maxbackupconfig.json");
+            }
+
+            if (!File.Exists(config_file_path)) {
+                context.Console.Error.WriteLine($"Config file not found at {config_file_path}");
+
+                return Task.FromResult(1);
+            }
+
+            // jobs is in the $.Backup.Jobs array
+
+            var json_options = new JsonSerializerOptions {
+                PropertyNameCaseInsensitive = false
+            };
+
+            var config = JsonSerializer.Deserialize<Config>(File.ReadAllText(config_file_path), json_options);
+
+            if (config?.Backup?.Jobs == null) {
+                context.Console.Error.WriteLine($"No jobs found in config file at {config_file_path}");
+
+                return Task.FromResult(1);
+            }
+
+            if (config.Backup.Jobs.Length == 0) {
+                context.Console.Error.WriteLine($"No jobs found in config file at {config_file_path}");
+
+                return Task.FromResult(1);
+            }
+
+            var table = new Table()
+                .Border(TableBorder.Rounded)
+                .BorderColor(Color.Red)
+                .AddColumn(new TableColumn("Name").Centered())
+                .AddColumn(new TableColumn("Source").Centered())
+                .AddColumn(new TableColumn("Destination").Centered())
+            ;
+
+            foreach (var job in config.Backup.Jobs) {
+                  table.AddRow(job.Name, job.Source, job.Destination);
+            }
+
+            AnsiConsole.Write(table);
+
+            return Task.FromResult(0);
+
+        }
+    }
+
+    public record Config(Backup Backup);
+    public record Backup(Job[] Jobs);
+    public record Job(string Name, string Source, string Destination, string[] Include, string[] Exclude);
+}
--- Max/Program.cs
+++ Max/Program.cs
@@ -1,22 +1,58 @@
-﻿using System.CommandLine.Builder;
+﻿// max status
+// shows the status of the service, the config file path, how many files were recently backed up, how many errors there have been recently, etc.
+// max start
+// starts the service
+// max stop
+// stops the service
+// max restart
+// restarts the service
+// max config
+// opens the config file in the default editor
+// max config --editor code-insiders // opens the config file in the specified editor
+// max config --editor code-insiders --wait // opens the config file in the specified editor and waits for the editor to close before continuing
+// max tail // tails the log file
+// max jobs // lists the jobs
+// max job create "My Job" "C:\Users\Me\Documents" "D:\Backups\Documents"
+// max job disable "My Job"
+// max job enable "My Job"
+// max job remove "My Job"
+// max job run "My Job"
+// max job run "My Job" --wait
+// max job run "My Job" --wait --verbose
+// max job run "My Job" --wait --verbose --dry-run
+
+using System.CommandLine.Builder;
 using System.CommandLine.Parsing;
+using System.CommandLine.Invocation;
 using System.CommandLine;
 using Max;
 using System.CommandLine.Help;
 using System.CommandLine.IO;
+using max;
 
-var delayOption = new Option<int>("--delay");
-var messageOption = new Option<string>("--message");
+// Max backup app for windows. This is the cli app that controls/configures the service.
 
-var rootCommand = new RootCommand("Middleware example");
-rootCommand.Add(delayOption);
-rootCommand.Add(messageOption);
+var default_config_path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "maxbackupconfig.json");
+
+var verboseOption = new Option<int>("--verbose");
+var configPathOption = new Option<string>(
+    "--config-path",
+    () => default_config_path,
+    "The path to the config file. Defaults to %USERPROFILE%\\maxbackupconfig.json"
+);
+
+var jobs_command = new JobsCommand(configPathOption);
+
+var rootCommand = new RootCommand("Max backup app for windows. This is the cli app that controls/configures the service.");
+rootCommand.Add(verboseOption);
+rootCommand.Add(configPathOption);
 rootCommand.SetHandler(context => {
     var con = new HelpContext(context.HelpBuilder, rootCommand, context.Console.Out.CreateTextWriter());
     context.HelpBuilder.Write(con);
 });
 
-rootCommand.Add(new ServiceCommand());
+rootCommand.AddCommand(jobs_command);
+rootCommand.AddCommand(new ServiceCommand());
 
 var commandLineBuilder = new CommandLineBuilder(rootCommand);
 
@@ -29,6 +65,7 @@ var commandLineBuilder = new CommandLineBuilder(rootCommand);
 //});
 
 commandLineBuilder.UseDefaults();
+
 var parser = commandLineBuilder.Build();
 
 var result = await parser.InvokeAsync(args);
